de5937b0ff7fc81e86f321123d250158e6e13bd5
cutieanya <morikono623@gmail.com>
2025-08-10 04:06:36 +0900

    ちょっと画面をいい感じにしてみました２

--- lib/chat_bubble.dart
+++ lib/chat_bubble.dart
@@ -57,7 +57,7 @@ class ChatBubble extends StatelessWidget {
           boxShadow: isMe
               ? [
             BoxShadow(
-              color: Colors.black.withOpacity(.06),
+              color: Colors.black.withValues(alpha: 0.06),
               blurRadius: 10,
               offset: const Offset(0, 2),
             ),
